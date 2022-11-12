6da83a305192fe42605bcd8b8be73ee9279f1654
Zismant <100025984+Zismant@users.noreply.github.com>
2022-11-12 22:07:55 +0200

    second

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     const fildElemts = Array.from(game.querySelectorAll('.fildElem'));
+    const btn = document.querySelector('#btn');
 
 
 
@@ -18,6 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let roteteStateElement = 0;
     let positionElement = 0;
     let lElement = [];
+    // let timerId = setInterval(moveDown, 1000);
 
     function createElement() {
         lElement = [
@@ -31,8 +33,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     }
 
-
-
     let tetrisElement;
 
     document.addEventListener('keydown', (e) => {
@@ -42,26 +42,32 @@ document.addEventListener('DOMContentLoaded', () => {
             moveRight();
         } else if (e.code == 'ArrowUp' || e.code == 'KeyW') {
             rotate();
-
         } else if (e.code == 'ArrowDown' || e.code == 'KeyS') {
+            moveDown();
+        }  
+    });
+    
+    btn.addEventListener('click', () => {
+        // clearInterval(timerId);
+        splice();
 
-        }
     });
 
     function moveLeft() {
-        positionElement--;
-        if (lElement[roteteStateElement].some(item => (item ) % 10 == 0)) {
-            positionElement++; 
+        
+        if (tetrisElement[roteteStateElement].some(item => item % cell == 0)) {
+            return;
         }
+        positionElement--;
         drawElement();
-
     }
 
     function moveRight() {
-        positionElement++;
-        if (lElement[roteteStateElement].some(item => (item - 9) % 10 == 0)) {
-            positionElement--; 
+
+        if (tetrisElement[roteteStateElement].some(item => (item - 9) % cell == 0)) {
+            return;
         }
+        positionElement++;
         drawElement();
 
     }
@@ -69,11 +75,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function rotate() {
         roteteStateElement++;
-        if (roteteStateElement == tetrisElement.length - 1) {
+        if (roteteStateElement == tetrisElement.length) {
             roteteStateElement = 0;
         }
         drawElement();
+        
 
+    }
+
+    function moveDown() {
+        positionElement += 10;
+        drawElement();
     }
 
     function clearElement() {
@@ -85,19 +97,47 @@ document.addEventListener('DOMContentLoaded', () => {
     function drawElement() {
         clearElement();
         createElement();
+        //left right collizion
+        // if (tetrisElement[roteteStateElement].some(item => item % 10 == 0)) {
+        //     positionElement++;
+        // } else if (tetrisElement[roteteStateElement].some(item => (item - 9) % 10 == 0)) {
+        //     positionElement--;
+        // }
+        // bottom collizion
+        let collizionBottom = tetrisElement[roteteStateElement].some(item => item >= 190 && item < 200  ||
+            fildElemts[item + cell].matches('.bottom')
+             );
 
+
+        if (collizionBottom ) {
+
+            tetrisElement[roteteStateElement].forEach(item => {
+
+                fildElemts[item].classList.add('bottom');
+                fildElemts[item].classList.remove('show');
+                positionElement = 0;
+                createElement();
+                return;
+            });
+
+        }
         tetrisElement[roteteStateElement].forEach(item => {
             fildElemts[item].classList.add('show');
         });
 
-        
-        console.log(tetrisElement[roteteStateElement]);
+
+
     }
 
+    
+   
 
- drawElement();
+    function splice() {
+        fildElemts.splice(10, 180);
+        console.log(fildElemts.length);
 
 
+    }
 
 
 
